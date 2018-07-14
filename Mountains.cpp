d9bc783310d6543189ea5e9a586977af0169c33f
Christos Patsouras <chpatsouras@gmail.com>
2018-07-14 13:41:53 +0300

    Mountains implementation

---
+++ Mountains.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Mountains.hpp"
+
+using namespace std ;
+
+
+Mountains::Mountains() {
+	cout << "Mountains were just created!" << endl ;
+}
+
+Mountains::~Mountains() {
+	cout << "Mountains are about to be destroyed" << endl ;
+}
---
+++ Mountains.hpp
@@ -0,0 +1,5 @@
+class Mountains {
+	public:
+	    Mountains();
+	    ~Mountains();
+};
