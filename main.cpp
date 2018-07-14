7d7ce03da99957edb299c7a97b7a4da368ad401b
Christos Patsouras <chpatsouras@gmail.com>
2018-07-14 13:37:33 +0300

    commit corrected main

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 
 
 int main(int argc, char const *argv[]) {
-	int L, N;
+	int L, Dist;
 	if (argc != 3) {
 		cout << "Incorrect amount of arguments" << endl;
+		cout << "You should give exactly two arguments (./IceSkater L Dist)" << endl;
+		cout << "L: Lake's length" << endl;
+		cout << "Dist: The distance that iceskater glides" << endl;
 		return -1;
 	}
 	L = atoi(argv[1]);
-	N = atoi(argv[2]);
-	cout << "L = " << L << " N = " << N << endl;
+	Dist = atoi(argv[2]);
+	Scene scene(L, Dist) ;
+	scene.plot() ;
     return 0;
 }
