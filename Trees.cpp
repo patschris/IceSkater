7142a4d17b1024b66daa4941c00a48df1b722d69
Christos Patsouras <chpatsouras@gmail.com>
2018-07-14 13:54:03 +0300

    Trees implementation

---
+++ Trees.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Trees.hpp"
+
+using namespace std;
+
+Trees::Trees() {
+	cout << "Trees were just created!" << endl ;
+}
+
+Trees::~Trees() {
+	cout << "Trees are about to be destroyed!" << endl ;
+}
---
+++ Trees.hpp
@@ -0,0 +1,5 @@
+class Trees {
+	public:
+	    Trees();
+	    ~Trees();
+};
